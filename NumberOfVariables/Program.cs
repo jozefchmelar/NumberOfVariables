aef7400239771b3d255ec1b56bed6dc2d74d792f
Jokinko <jozefchmelar@users.noreply.github.com>
2021-07-29 09:26:07 +0200

    Init

---
+++ NumberOfVariables/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Plc;
+using Vortex.Connector.ValueTypes;
+
+namespace NumberOfVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var shouldBe5 = NumberOfVariables(typeof(StructWith5Variables));
+            var shouldBe68 = NumberOfVariables(typeof(StructWith68Variables));
+            var shouldBe50 = NumberOfVariables(typeof(StructWith50Variables));
+            Console.WriteLine(shouldBe5);
+            Console.WriteLine(shouldBe68);
+            Console.WriteLine(shouldBe50);
+
+            //write the values to the plc
+            Entry.Plc.Connector.BuildAndStart();
+            Entry.Plc.MAIN.ShouldBe5.Cyclic = (short)shouldBe5;
+            Entry.Plc.MAIN.ShouldBe68.Cyclic = (short)shouldBe68;
+            Entry.Plc.MAIN.ShouldBe50.Cyclic = (short)shouldBe50;
+            Console.Read();
+        }
+        private static int NumberOfVariables(Type ofType) => ofType
+            .GetProperties()
+            .Where(x => typeof(OnlinerBaseType).IsAssignableFrom(x.PropertyType))
+            .Count();
+
+    }
+
+}
---
+++ PlcConnector/Entry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Vortex.Adapters.Connector.Tc3.Adapter;
+
+namespace Plc
+{
+    public static class Entry
+    {
+        public static Plc.PlcTwinController Plc { get; }= new Plc.PlcTwinController(Tc3ConnectorAdapter.Create(null, 851, true));
+    }
+}   
